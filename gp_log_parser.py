4bf827ac66341c220cba72ff0b20dccaf3a22cc8
Eric <erlaplante@gmail.com>
2021-01-06 23:17:48 -0800

    Included check to not create pivot table if dataframe has no errors, only retain file name from passed log, and minor refactoring.

--- gp_log_parser.py
+++ gp_log_parser.py
@@ -3,24 +3,32 @@ import sys
 import pandas as pd
 
 with open(sys.argv[1], 'r') as logFile:
-    fname = sys.argv[1].lower()
-    # print(fname)  * REMOVE full directory/path i.e. only include filename
-    if ('event' in fname) and (('gps' not in fname) and ('gpa' not in fname)):
-        logType = 'event'
-    elif ('gps' in fname) and (('event' not in fname) and ('gpa' not in fname)):
-        logType = 'gps'
-    elif ('gpa' in fname) and (('event' not in fname) and ('gps' not in fname)):
-        logType = 'gpa'
-    else:
-        print("* Log file name did not include a log file keyword ('event', 'GPS', or 'GPA').\n"
-              "Or multiple keywords were found.\n\n"
-              "Enter a number for your log file type:\n"
-              "(1) pan_gp_event.log\n"
-              "(2) PanGPS.log\n"
-              "(3) PanGPA.log")
     logData = logFile.read()
 
-if logType == 'event':
+# Only retain file name portion of passed in log file.
+sub_path = re.compile(r'.*[\\\/]')
+fname = sub_path.sub('', sys.argv[1].lower())
+
+# Detect keyword in file name to determine log type, otherwise ask for type.
+if ('event' in fname) and (('gps' not in fname) and ('gpa' not in fname)):
+    logType = 'Event_Log'
+elif ('gps' in fname) and (('event' not in fname) and ('gpa' not in fname)):
+    logType = 'GPS_Log'
+elif ('gpa' in fname) and (('event' not in fname) and ('gps' not in fname)):
+    logType = 'GPA_Log'
+else:
+    selections = "Enter a number for your log file type:\n(1) pan_gp_event.log\n(2) PanGPS.log\n(3) PanGPA.log\n> "
+
+    logNumInput = input("Log file name did not include a log file keyword ('event', 'GPS', or 'GPA').\n"
+                        "Or multiple keywords were found.\n\n" + selections)
+    while logNumInput not in ['1', '2', '3']:
+        logNumInput = input("Invalid Response\n\n" + selections)
+
+    logType = {'1': 'Event_Log',
+               '2': 'GPS_Log',
+               '3': 'GPA_Log'}.get(logNumInput)
+
+if logType == 'Event_Log':
     # Regex for two digit year substitution.
     sub_pattern = re.compile(r'(\d\d\/\d\d\/)\d\d(\d\d)')
     
@@ -28,13 +36,13 @@ if logType == 'event':
     pattern = re.compile(r'(\d\d\/\d\d\/\d\d) (\d\d):(\d\d):(\d\d\.\d\d\d) \[(\w+)\s?\]: (.*)')
     
     # Change four digit year to two.
-    subbedData = sub_pattern.sub(r'\1\2', logData)   
+    subbedData = sub_pattern.sub(r'\1\2', logData)
     
     # Make a list of tuples from 'subbedData' and create DataFrame.
     data = pattern.findall(subbedData)
     df = pd.DataFrame(data, columns = ['Date', 'HH', 'MM', 'SS.SSS', 'Type', 'LogOutput'])
 else:
-    # Regex for 'PanGPS.log' columns.
+    # Regex for 'PanGPS.log' and 'PanGPA.log' columns.
     pattern = re.compile(r'(\(T\d+\))(\w+)\s*(\(\s*\d+\)):\s(\d\d\/\d\d\/\d\d)\s(\d\d):(\d\d):(\d\d:\d\d\d)\s(.*)')
 
     # Make a list of tuples from 'logData' and create DataFrame.
@@ -44,14 +52,17 @@ else:
 print('Summarized log output:\n')
 print(df)
 
-# Pivot on count of LogOutput Errors by Date.
-# * check for case where there are no erros to pivot on
-pivot_log_errors = pd.pivot_table(df[df.Type == 'Error'], index='LogOutput', columns='Date', values='Type', aggfunc='count')
+writer = pd.ExcelWriter('PyOut_' + logType + '.xlsx')
+df.to_excel(writer, logType)
 
-writer = pd.ExcelWriter('GP_Log_PyOut.xlsx')
-df.to_excel(writer, 'GP Log')
-pivot_log_errors.to_excel(writer, 'Pivot - Errors')
+print("\nFull parsed output saved as 'PyOut_" + logType + ".xlsx' in current path.")
+
+if df.query('Type=="Error"').empty:
+    print("No pivot table created because there were no Errors in the log data.")    
+else:
+    # Pivot on count of LogOutput Errors by Date.
+    pivot_log_errors = pd.pivot_table(df[df.Type == 'Error'], index='LogOutput', columns='Date', values='Type', aggfunc='count', margins=True)
+    pivot_log_errors.to_excel(writer, 'Pivot_Errors')
+    print("Second sheet includes Pivot Table on the count of LogOutput Errors by Date.")
+
 writer.save()
-
-print("\nFull parsed output saved as 'GP_Log_PyOut.xlsx' in current path.\n"
-      "Second sheet includes Pivot Table on the count of LogOutput Errors by Date.")
